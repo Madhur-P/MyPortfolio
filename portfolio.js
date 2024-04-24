9c1d4448c3dd3c56c6c63fc5ef73f265b4997baf
Madhur <132658343+Madhur-P@users.noreply.github.com>
2024-04-24 16:55:06 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,18 @@
+function cert1() {
+    window.location.href = "https://www.coursera.org/account/accomplishments/verify/UML8RC8VLA2N";
+  }
+  
+  function cert2() {
+    
+    window.location.href = "https://www.udemy.com/certificate/UC-8a22ba68-cb17-446c-8325-598f7085ccc0/";
+    
+  }
+  function cert3() {
+    window.location.href = "https://www.hackerrank.com/certificates/cee9c34fc4d2";
+  }
+  
+  function cert4() {
+    
+    window.location.href = "https://www.coursera.org/account/accomplishments/specialization/certificate/JSZBTFT9Q674";
+    
+  }
